dc7493ac61a94df1d1a81b68445203fcab504e40
Andres Garrido <buster092@gmail.com>
2015-11-02 17:56:07 -0300

    updated async task to retrieve results to login activity

--- quantimodo-sdk-tools/src/main/java/com/quantimodo/tools/utils/GetUsernameTask.java
+++ quantimodo-sdk-tools/src/main/java/com/quantimodo/tools/utils/GetUsernameTask.java
@@ -1,6 +1,5 @@
 package com.quantimodo.tools.utils;
 
-import android.app.Activity;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -8,21 +7,18 @@ import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.auth.UserRecoverableAuthException;
 import com.google.gson.JsonObject;
-import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
-import com.koushikdutta.ion.Response;
 import com.quantimodo.tools.ToolsPrefs;
 import com.quantimodo.tools.activities.QuantimodoLoginActivity;
-import com.quantimodo.tools.sdk.AuthHelper;
 
 import java.io.IOException;
-
-import javax.inject.Inject;
+import java.util.concurrent.ExecutionException;
 
 /**
- * Created by Andres on 30-10-2015.
+ * AsynkTask to fetch the google token and handling the result
  */
-public class GetUsernameTask extends AsyncTask<Void, Void, Void> {
+public class GetUsernameTask extends AsyncTask<Void, Void, JsonObject> {
+    private static final String TAG = GetUsernameTask.class.getSimpleName();
 
     ToolsPrefs mPrefs;
 
@@ -42,58 +38,46 @@ public class GetUsernameTask extends AsyncTask<Void, Void, Void> {
      * on the AsyncTask instance.
      */
     @Override
-    protected Void doInBackground(Void... params) {
+    protected JsonObject doInBackground(Void... params) {
         try {
             String token = fetchToken();
             if (token != null) {
-                // **Insert the good stuff here.**
-                // Use the token to access the user's Google data.
-                Log.d("GetUsernameTask", "token correcto!!: " + token);
+                Log.d(TAG, "correct token!!: " + token);
+                try {
+                    return Ion.with(mActivity)
+                            .load(mPrefs.getApiSocialAuth())
+                            .setBodyParameter("provider", "google")
+                            .setBodyParameter("accessToken", token)
+                            .asJsonObject()
+                            .get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
 
-                Ion.with(mActivity)
-                        .load(mPrefs.getApiSocialAuth())
-                        .setBodyParameter("provider", "google")
-                        .setBodyParameter("accessToken", token)
-                        .asString()
-                        .withResponse().setCallback(new FutureCallback<Response<String>>() {
-                    @Override
-                    public void onCompleted(Exception e, Response<String> response) {
-                        if (e != null) {
-                            e.printStackTrace();
-                            return;
-                        }
-                        String result = response.getResult();
-//                        JsonObject result = response.getResult();
-                        if (result != null) {
-                            try {
-                                Log.d("GetUsernameTask", "json result!: " + result.toString());
-//                                String accessToken = result.get("access_token").getAsString();
-//                                String refreshToken = result.get("refresh_token").getAsString();
-//                                int expiresIn = result.get("expires_in").getAsInt();
-
-//                                authHelper.setAuthToken(new AuthHelper.AuthToken(accessToken, refreshToken, System.currentTimeMillis() / 1000 + expiresIn));
-
-//                                finish();
-                            } catch (NullPointerException ignored) {
-                                Log.i(ToolsPrefs.DEBUG_TAG, "Error getting access token: ");
-                            }
-                        } else {
-                            Log.d("GetUsernameTask", "result nulo, response.toString: " + response.toString());
-
-                        }
-                    }
-                });
             }
             else
-                Log.d("GetUsernameTask", "token null :( ");
+                Log.d(TAG, "token null :( ");
         } catch (IOException e) {
             // The fetchToken() method handles Google-specific exceptions,
             // so this indicates something went wrong at a higher level.
-            // TIP: Check for network connectivity before starting the AsyncTask.
             e.printStackTrace();
         }
         return null;
     }
+
+    @Override
+    protected void onPostExecute(JsonObject result) {
+        if(result != null) {
+            final String accessToken = result.get("access_token").getAsString();
+            final String refreshToken = result.get("refresh_token").getAsString();
+            final int expiresIn = result.get("expires_in").getAsInt();
+            mActivity.setAuthToken(accessToken, refreshToken, expiresIn);
+        }
+        else{
+            Log.e(TAG, "result from QM server was null!");
+        }
+    }
+
 
     /**
      * Gets an authentication token from Google and handles any
