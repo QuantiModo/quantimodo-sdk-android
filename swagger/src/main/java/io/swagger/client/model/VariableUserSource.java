5e61e12c50063338e39fbab74d4b7bdf3b0b608b
Mike P. Sinn <m@quantimodo.com>
2015-11-20 12:21:00 -0600

    Made start_time a string not integer

--- swagger/src/main/java/io/swagger/client/model/VariableUserSource.java
+++ swagger/src/main/java/io/swagger/client/model/VariableUserSource.java
@@ -100,9 +100,9 @@ public class VariableUserSource  {
 
   
   /**
-   * When the measurement record was first created in ISO 8601 datetime format
+   * When the record was first created. Use ISO 8601 datetime format
    **/
-  @ApiModelProperty(value = "When the measurement record was first created in ISO 8601 datetime format")
+  @ApiModelProperty(value = "When the record was first created. Use ISO 8601 datetime format")
   public Date getCreatedAt() {
     return createdAt;
   }
@@ -112,9 +112,9 @@ public class VariableUserSource  {
 
   
   /**
-   * When the measurement record in the database was last updated. Use ISO 8601 datetime format
+   * When the record in the database was last updated. Use ISO 8601 datetime format
    **/
-  @ApiModelProperty(value = "When the measurement record in the database was last updated. Use ISO 8601 datetime format")
+  @ApiModelProperty(value = "When the record in the database was last updated. Use ISO 8601 datetime format")
   public Date getUpdatedAt() {
     return updatedAt;
   }
