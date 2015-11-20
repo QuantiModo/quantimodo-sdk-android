504d04400890018d58f2fc21c2b274ead21ae2d0
Mike P. Sinn <m@quantimodo.com>
2015-11-20 09:40:30 -0600

    Documented created_at and updated_at parameters

--- swagger/src/main/java/io/swagger/client/model/Source.java
+++ swagger/src/main/java/io/swagger/client/model/Source.java
@@ -58,9 +58,9 @@ public class Source  {
 
   
   /**
-   * created_at
+   * When the measurement record was first created in ISO 8601 datetime format
    **/
-  @ApiModelProperty(value = "created_at")
+  @ApiModelProperty(value = "When the measurement record was first created in ISO 8601 datetime format")
   public Date getCreatedAt() {
     return createdAt;
   }
@@ -70,9 +70,9 @@ public class Source  {
 
   
   /**
-   * updated_at
+   * When the measurement record in the database was last updated. Use ISO 8601 datetime format
    **/
-  @ApiModelProperty(value = "updated_at")
+  @ApiModelProperty(value = "When the measurement record in the database was last updated. Use ISO 8601 datetime format")
   public Date getUpdatedAt() {
     return updatedAt;
   }
--- swagger/src/main/java/io/swagger/client/model/VariableCategory.java
+++ swagger/src/main/java/io/swagger/client/model/VariableCategory.java
@@ -188,9 +188,9 @@ public class VariableCategory  {
 
   
   /**
-   * created_at
+   * When the measurement record was first created in ISO 8601 datetime format
    **/
-  @ApiModelProperty(value = "created_at")
+  @ApiModelProperty(value = "When the measurement record was first created in ISO 8601 datetime format")
   public Date getCreatedAt() {
     return createdAt;
   }
@@ -200,9 +200,9 @@ public class VariableCategory  {
 
   
   /**
-   * updated_at
+   * When the measurement record in the database was last updated. Use ISO 8601 datetime format
    **/
-  @ApiModelProperty(value = "updated_at")
+  @ApiModelProperty(value = "When the measurement record in the database was last updated. Use ISO 8601 datetime format")
   public Date getUpdatedAt() {
     return updatedAt;
   }
