53601ab698205d07f6cef8be3d6017702245be9d
andres <andres>
2016-01-20 21:50:08 -0300

    added loading dialog while deleting reminder

--- quantimodo-sdk-tools/src/main/java/com/quantimodo/tools/activities/CustomRemindersCreateActivity.java
+++ quantimodo-sdk-tools/src/main/java/com/quantimodo/tools/activities/CustomRemindersCreateActivity.java
@@ -2,6 +2,7 @@ package com.quantimodo.tools.activities;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -69,6 +70,7 @@ public class CustomRemindersCreateActivity extends Activity {
     private View containerLayout2;
     private View containerCategories;
     private View buttonsLayout;
+    private ProgressDialog mProgress;
 
     private AutoCompleteListAdapter autoCompleteListAdapter;
     private ArrayList<Variable> suggestedVariables = new ArrayList<>();
@@ -448,6 +450,10 @@ public class CustomRemindersCreateActivity extends Activity {
             ).show();
             return;
         }
+        mProgress = new ProgressDialog(this);
+        mProgress.setIndeterminate(true);
+        mProgress.setMessage(getString(R.string.please_wait_message));
+        mProgress.show();
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -478,6 +484,9 @@ public class CustomRemindersCreateActivity extends Activity {
                             apiException.getMessage().toLowerCase().contains("unauthorized")) {
                         deleteLocally();
                     }
+                    else{
+                        showErrorMessage();
+                    }
                 }
             }
         });
@@ -485,6 +494,7 @@ public class CustomRemindersCreateActivity extends Activity {
     }
 
     private void showErrorMessage(){
+        if(mProgress != null && mProgress.isShowing()) mProgress.dismiss();
         runOnUiThread(new Runnable() {
             public void run() {
                 Toast.makeText(
@@ -497,6 +507,7 @@ public class CustomRemindersCreateActivity extends Activity {
     }
 
     private void deleteLocally(){
+        if(mProgress != null && mProgress.isShowing()) mProgress.dismiss();
         CustomRemindersHelper.removeReminder(CustomRemindersCreateActivity.this,
                 reminderId);
         runOnUiThread(new Runnable() {
