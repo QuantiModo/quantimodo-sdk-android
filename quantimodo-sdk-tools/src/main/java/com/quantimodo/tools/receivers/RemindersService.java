fd1386dfc1ac416bfe9f8eb365563a44123bf4e9
Aleksey Shilin <toseter.lex@gmail.com>
2016-03-10 15:14:07 +0300

    fix reminders send

--- quantimodo-sdk-tools/src/main/java/com/quantimodo/tools/receivers/RemindersService.java
+++ quantimodo-sdk-tools/src/main/java/com/quantimodo/tools/receivers/RemindersService.java
@@ -7,17 +7,33 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import com.quantimodo.android.sdk.QuantimodoApiV2;
+import com.quantimodo.android.sdk.login.ToolsPrefs;
+import com.quantimodo.android.sdk.model.Measurement;
+import com.quantimodo.android.sdk.model.MeasurementSet;
+import com.quantimodo.tools.QApplication;
 import com.quantimodo.tools.QTools;
 import com.quantimodo.tools.R;
+import com.quantimodo.tools.sdk.AuthHelper;
+import com.quantimodo.tools.sdk.request.NoNetworkConnection;
 import com.quantimodo.tools.utils.CustomRemindersHelper;
 import com.quantimodo.tools.utils.QtoolsUtils;
 
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
+
+import static com.quantimodo.tools.receivers.CustomRemindersReceiver.EXTRA_ACTION;
+import static com.quantimodo.tools.utils.CustomRemindersHelper.EXTRA_REMINDER_ID;
+import static com.quantimodo.tools.utils.CustomRemindersHelper.EXTRA_SPECIFIC_ID;
 
 /**
  * This {@code IntentService} does the app's actual work.
@@ -29,23 +45,77 @@ import java.util.Random;
 public class RemindersService extends IntentService {
     private static final String TAG = RemindersService.class.getSimpleName();
 
+    public static final String ACTION_NOTIFICATION = "notification";
+    public static final String ACTION_SEND_VALUE = "send";
+
+    @Inject
+    AuthHelper mAuthHelper;
+
+    @Inject
+    protected QuantimodoApiV2 mClient;
+
     public RemindersService() {
         super("RemindersService");
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        QTools.getInstance().inject(this);
+    }
+
+    @Override
     protected void onHandleIntent(Intent intent) {
-        if(intent.hasExtra(CustomRemindersHelper.EXTRA_REMINDER_ID)){
-            String reminderId = intent.getExtras().getString(
-                    CustomRemindersHelper.EXTRA_REMINDER_ID);
-            int specificId = intent.getExtras().getInt(CustomRemindersHelper.EXTRA_SPECIFIC_ID);
-            CustomRemindersHelper.Reminder reminder = CustomRemindersHelper.getReminder(this, reminderId);
-            sendNotification(reminder, specificId);
+        Bundle extras = intent.getExtras();
+        if (extras == null || !extras.containsKey(EXTRA_REMINDER_ID)){
+            CustomRemindersReceiver.completeWakefulIntent(intent);
+            Log.d(TAG,"Check intent");
+            return;
         }
-        else
-            Log.d(TAG, "onHandleIntent has no extras");
-        // Release the wake lock provided by the BroadcastReceiver.
-        CustomRemindersReceiver.completeWakefulIntent(intent);
+
+
+        try {
+            String reminderId = intent.getExtras().getString(EXTRA_REMINDER_ID);
+            int specificId = intent.getExtras().getInt(EXTRA_SPECIFIC_ID);
+            String action = extras.getString(EXTRA_ACTION, ACTION_NOTIFICATION);
+
+            CustomRemindersHelper.Reminder reminder = CustomRemindersHelper.getReminder(this, reminderId);
+
+            switch (action) {
+                case ACTION_NOTIFICATION:
+                    sendNotification(reminder, specificId);
+                    break;
+
+                case ACTION_SEND_VALUE:
+                    sendData(reminder,specificId);
+                    break;
+            }
+        } finally {
+            CustomRemindersReceiver.completeWakefulIntent(intent);
+        }
+    }
+
+    private void sendData(CustomRemindersHelper.Reminder reminder, int specificId){
+        try {
+            String token = mAuthHelper.getAuthTokenWithRefresh();
+
+            MeasurementSet set = new MeasurementSet(
+                    reminder.name,
+                    null,
+                    reminder.variableCategory,
+                    reminder.unitName,
+                    reminder.combinationOperation,"MoodiModo");
+
+            set.getMeasurements().add(new Measurement(System.currentTimeMillis() / 1000,Double.parseDouble(reminder.value)));
+
+            ArrayList<MeasurementSet> sets = new ArrayList<>();
+            sets.add(set);
+
+            mClient.putMeasurements(this,token,sets);
+            Log.d(TAG,"Measurement sent");
+        } catch (NoNetworkConnection noNetworkConnection) {
+            noNetworkConnection.printStackTrace();
+        }
     }
 
     /**
@@ -64,16 +134,16 @@ public class RemindersService extends IntentService {
 
 
         snoozeIntent.putExtra(CustomRemindersReceiver.EXTRA_NOTIFICATION_ID, specificId);
-        snoozeIntent.putExtra(CustomRemindersHelper.EXTRA_REMINDER_ID, reminderId);
+        snoozeIntent.putExtra(EXTRA_REMINDER_ID, reminderId);
         snoozeIntent.putExtra(CustomRemindersReceiver.EXTRA_REQUEST_SNOOZE, true);
         trackIntent.putExtra(CustomRemindersReceiver.EXTRA_NOTIFICATION_ID, specificId);
-        trackIntent.putExtra(CustomRemindersHelper.EXTRA_REMINDER_ID, reminderId);
+        trackIntent.putExtra(EXTRA_REMINDER_ID, reminderId);
         trackIntent.putExtra(CustomRemindersReceiver.EXTRA_REQUEST_REMINDER, true);
         editIntent.putExtra(CustomRemindersReceiver.EXTRA_NOTIFICATION_ID, specificId);
-        editIntent.putExtra(CustomRemindersHelper.EXTRA_REMINDER_ID, reminderId);
+        editIntent.putExtra(EXTRA_REMINDER_ID, reminderId);
         editIntent.putExtra(CustomRemindersReceiver.EXTRA_REQUEST_EDIT, true);
         popupIntent.putExtra(CustomRemindersReceiver.EXTRA_NOTIFICATION_ID, specificId);
-        popupIntent.putExtra(CustomRemindersHelper.EXTRA_REMINDER_ID, reminderId);
+        popupIntent.putExtra(EXTRA_REMINDER_ID, reminderId);
         popupIntent.putExtra(CustomRemindersReceiver.EXTRA_REQUEST_POPUP, true);
 
         Random rnd = new Random();
